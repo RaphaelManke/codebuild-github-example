540443d04c98b9614928e5fd460750c9518836ad
Raphael Manke <github@manke.cloud>
2024-05-03 10:27:34 +0200

    add secret

--- aws-setup/lib/aws-setup-stack.ts
+++ aws-setup/lib/aws-setup-stack.ts
@@ -6,6 +6,9 @@ export class AwsSetupStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const secret = new cdk.aws_secretsmanager.Secret(this, "some-secret", {
+      secretName: "some-secret-value",
+    });
     new cdk.aws_codebuild.Project(this, "Github", {
       projectName: "github-runner",
       source: cdk.aws_codebuild.Source.gitHub({
@@ -22,6 +25,12 @@ export class AwsSetupStack extends cdk.Stack {
       environment: {
         computeType: cdk.aws_codebuild.ComputeType.SMALL,
         buildImage: cdk.aws_codebuild.LinuxBuildImage.STANDARD_7_0,
+      },
+      environmentVariables: {
+        CUSTOM_SECRET: {
+          type: cdk.aws_codebuild.BuildEnvironmentVariableType.SECRETS_MANAGER,
+          value: "some-secret-value",
+        },
       },
     });
   }
