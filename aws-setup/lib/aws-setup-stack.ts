e86cc86f70791a24ef9f4be5281c02ae004e4979
Raphael Manke <github@manke.cloud>
2024-05-03 10:13:04 +0200

    init

---
+++ aws-setup/lib/aws-setup-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsSetupStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new cdk.aws_codebuild.Project(this, "Github", {
+      source: cdk.aws_codebuild.Source.gitHub({
+        owner: "RaphaelManke",
+        repo: "codebuild-github-example",
+        webhook: true,
+        webhookFilters: [
+          cdk.aws_codebuild.FilterGroup.inEventOf(
+            // @ts-ignore
+            "WORKFLOW_JOB_QUEUED"
+          ),
+        ],
+      }),
+      environment: {
+        computeType: cdk.aws_codebuild.ComputeType.SMALL,
+        buildImage: cdk.aws_codebuild.LinuxBuildImage.STANDARD_7_0,
+      },
+    });
+  }
+}
